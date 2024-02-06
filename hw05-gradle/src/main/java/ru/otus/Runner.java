4293de2f6aca9522ec351ea65df616f0e91c1658
{Dantsy} <{dantsy@yandex.ru}>
2024-02-06 12:30:33 +0300

    Hw5

---
+++ hw05-gradle/src/main/java/ru/otus/Runner.java
@@ -0,0 +1,68 @@
+package ru.otus;
+
+import ru.otus.annotations.After;
+import ru.otus.annotations.Before;
+import ru.otus.annotations.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Runner {
+
+    public static void runTests(String testClassName) {
+        try {
+            Class<?> testClass = Class.forName(testClassName);
+            Object testObject = testClass.getDeclaredConstructor().newInstance();
+
+            Method[] methods = testClass.getMethods();
+            List<Method> beforeMethods = new ArrayList<>();
+            List<Method> testMethods = new ArrayList<>();
+            List<Method> afterMethods = new ArrayList<>();
+
+            for (Method method : methods) {
+                if (method.isAnnotationPresent(Before.class)) {
+                    beforeMethods.add(method);
+                } else if (method.isAnnotationPresent(Test.class)) {
+                    testMethods.add(method);
+                } else if (method.isAnnotationPresent(After.class)) {
+                    afterMethods.add(method);
+                }
+            }
+
+            int successfulTests = 0;
+            int failedTests = 0;
+
+            for (Method testMethod : testMethods) {
+                try {
+                    invokeMethods(beforeMethods, testObject);
+                    testMethod.invoke(testObject);
+                    invokeMethods(afterMethods, testObject);
+                    successfulTests++;
+                } catch (Exception e) {
+                    failedTests++;
+                    e.printStackTrace();
+                }
+            }
+
+            System.out.println("Пройденный: " + successfulTests);
+            System.out.println("Ошибка: " + failedTests);
+            System.out.println("Результат: " + (successfulTests + failedTests));
+
+        } catch (ClassNotFoundException | NoSuchMethodException | InstantiationException | IllegalAccessException |
+                 InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void invokeMethods(List<Method> methods, Object testObject) throws IllegalAccessException, InvocationTargetException {
+        for (Method method : methods) {
+            method.invoke(testObject);
+        }
+    }
+
+    public static void main(String[] args) {
+        Runner.runTests("ru.otus.Testing");
+    }
+}
---
+++ hw05-gradle/src/main/java/ru/otus/Testing.java
@@ -0,0 +1,23 @@
+package ru.otus;
+
+import ru.otus.annotations.After;
+import ru.otus.annotations.Before;
+import ru.otus.annotations.Test;
+
+public class Testing {
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void testMethod1() {
+    }
+
+    @Test
+    public void testMethod2() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+}
